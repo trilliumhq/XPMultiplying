8c333f1d8a0c685aedba24db8ff5b9445b47b194
iapchi <48498436+iapchi@users.noreply.github.com>
2022-01-25 22:38:23 -0500

    Luckperms implementation of exp multiplier from exp orbs.

--- src/main/java/com/trilliumhq/multiplying/Multiplying.java
+++ src/main/java/com/trilliumhq/multiplying/Multiplying.java
@@ -1,5 +1,15 @@
 package com.trilliumhq.multiplying;
 
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.cacheddata.CachedMetaData;
+import net.luckperms.api.cacheddata.CachedPermissionData;
+import net.luckperms.api.model.group.Group;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.Node;
+import net.luckperms.api.query.QueryOptions;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -7,6 +17,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerExpChangeEvent;
 import org.bukkit.permissions.PermissionAttachmentInfo;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Iterator;
+import java.util.Objects;
 
 public final class Multiplying extends JavaPlugin implements Listener {
 
@@ -21,8 +34,10 @@ public final class Multiplying extends JavaPlugin implements Listener {
     @EventHandler(priority = EventPriority.HIGH)
     public void onXPPickup(PlayerExpChangeEvent event) {
         Player p = event.getPlayer();
-        String permissionMultipler = p.getEffectivePermissions().stream().map(PermissionAttachmentInfo::getPermission).filter(permission -> permission.startsWith("expmultiplier.amount.")).findFirst().get();
-        event.setAmount(event.getAmount() * Integer.parseInt(permissionMultipler.substring(permissionMultipler.lastIndexOf(".") + 1)));
+//        p.sendMessage(String.valueOf(metaData.getMetaValue("exp", Integer::parseInt).orElse(1)));
+        if(Objects.requireNonNull(event.getSource()).getType().equals(EntityType.EXPERIENCE_ORB)){
+            event.setAmount(event.getAmount() * LuckPermsProvider.get().getPlayerAdapter(Player.class).getMetaData(p).getMetaValue("exp", Integer::parseInt).orElse(1));
+        }
     }
     @Override
     public void onDisable() {
